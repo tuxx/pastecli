9f4895c8fda9a0d14bdd7eaeb03effaec9018241
Ben Oswald <ben.oswald@root-space.de>
2013-12-13 22:17:17 +0100

    add the basic functionality
    and some comments

--- paste_client.py
+++ paste_client.py
@@ -1,14 +1,24 @@
 import json
 import requests
+import sys
 
+# set the paste service api url
 url = 'http://paste.0x06.org/api/json/create'
 
-payload = {'data':'foo','language':'text'}
+# read stuff from stdin
+data = sys.stdin.read()
 
+# build the payload
+payload = {'data':data,'language':'text'}
+
+# some headers
 headers = {'content-type': 'application/json'}
 
+# send the data to paste service
 r = requests.post(url, data=json.dumps(payload), headers=headers)
 
+# parse the json answer
 res = json.loads(r.text)
 
+# print the paste url
 print("http://paste.0x06.org/" + res['result']['id'])
