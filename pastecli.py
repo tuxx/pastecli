9adbe03ee77cb83cd41786dd1f5072fcb3291794
Ben Oswald <ben.oswald@root-space.de>
2013-12-16 00:12:04 +0100

    add the code to use the new default option 'expire' for the config file

--- pastecli.py
+++ pastecli.py
@@ -9,12 +9,13 @@ config.read('pastecli.conf')
 url = config['SERVICE']['url']
 api = config['SERVICE']['api']
 lang = config['OPTIONS']['lang']
+expire = config['OPTIONS']['expire']
 
 # read stuff from stdin
 data = sys.stdin.read()
 
 # build the payload
-payload = {'data':data,'language':lang}
+payload = {'data':data,'language':lang,'expire':expire}
 
 # some headers
 headers = {'content-type': 'application/json'}
