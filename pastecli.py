ef6231af1c8aee8e4bec9322d263282da4282c5d
Ben Oswald <ben.oswald@root-space.de>
2013-12-17 23:57:50 +0100

    implement command line parsing and error detection

--- pastecli.py
+++ pastecli.py
@@ -3,6 +3,7 @@ import requests
 import sys
 import configparser
 from os.path import expanduser
+import argparse
 
 def get_config():
     user_conf = expanduser("~") + '/.pastecli.conf'
@@ -24,25 +25,64 @@ if __name__ == "__main__":
     # read config file
     config = configparser.ConfigParser()
     config.read(get_config())
-    url = config['SERVICE']['url']
+    # argument parsing
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t','--title',help='set the title of the paste',dest='title', default=None)
+    parser.add_argument('-l','--lang',help='set the systax highlighting of the paste',dest='lang',default=config['OPTIONS']['lang'])
+    parser.add_argument('--private',help='set if the paste should be private of the paste',action='store_true',dest='private', default=None)
+    parser.add_argument('-p','--password',help='set the password for the paste',dest='password', default=None)
+    parser.add_argument('-e','--expire',help='set the expire time of the paste in seconds',dest='expire', default=config['OPTIONS']['expire'])
+    parser.add_argument('-u','--url',help='set the url of the paste service',dest='url', default=config['SERVICE']['url'] )
+    args = parser.parse_args()
+
     api = 'api/json/'
-    lang = config['OPTIONS']['lang']
-    expire = config['OPTIONS']['expire']
+
+    # check args
+    r = requests.get(args.url+ api + 'parameter/language')
+    res = json.loads(r.text)
+    if args.lang not in res['result']['values']:
+        print("the given language isn't valid please use one of these")
+        print([x.encode('utf8') for x in res['result']['values']])
+        exit()
+    del r
+    del res
+
+    r = requests.get(args.url+ api + 'parameter/expire')
+    res = json.loads(r.text)
+    if int(args.expire) not in res['result']['values']:
+        print("the given expire time isn't valide please use one of these")
+        print(res['result']['values'])
+        exit()
+    del r
+    del res
 
     # read stuff from stdin
     data = sys.stdin.read()
 
     # build the payload
-    payload = {'data':data,'language':lang,'expire':expire}
+    payload = {'data':data,'language':args.lang,'expire':args.expire}
+
+    if args.title is not None:
+        payload.update({'title':args.title})
+
+    if args.private is not None:
+        payload.update({'private':args.private})
+
+    if args.password is not None:
+        payload.update({'password':args.password})
+
 
     # some headers
     headers = {'content-type': 'application/json'}
 
     # send the data to paste service
-    r = requests.post(url + api + 'create', data=json.dumps(payload), headers=headers)
+    r = requests.post(args.url + api + 'create', data=json.dumps(payload), headers=headers)
 
     # parse the json answer
     res = json.loads(r.text)
 
     # print the paste url
-    print(url + res['result']['id'])
+    if res['result']['hash'] is not None:
+        print(args.url + res['result']['id'] + '/' + res['result']['hash'])
+    else:
+        print(args.url + res['result']['id'])
